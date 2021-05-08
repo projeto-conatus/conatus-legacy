7c3eb7e14b1a36e3ebacde5b3b9c4f481c8df17c
Marcos <marcos-rj@outlook.com>
2021-05-08 17:33:42 -0300

    primeiros passos

---
+++ js/api/equipe.js
@@ -0,0 +1,54 @@
+const equipe = [
+  {
+    id: 1,
+    nome: "Carlos Eduardo",
+    foto: "./assets/photos/carlos.png",
+    funcao: "Desenvolvedor",
+    linkedin: "",
+    github: "",
+  },
+
+  {
+    id: 2,
+    nome: "Larissa Firmino",
+    foto: "./assets/photos/larissa.png",
+    funcao: "Desenvolvedor",
+    linkedin: "",
+    github: "",
+  },
+
+  {
+    id: 3,
+    nome: "Marcos Antonio",
+    foto: "./assets/photos/marcos.png",
+    funcao: "Desenvolvedor",
+    linkedin: "",
+    github: "",
+  },
+  {
+    id: 4,
+    nome: "Midian Brandão",
+    foto: "./assets/photos/midian.png",
+    funcao: "Desenvolvedor",
+    linkedin: "",
+    github: "",
+  },
+  {
+    id: 5,
+    nome: "Nathalia Dantas",
+    foto: "./assets/photos/nathalia.png",
+    funcao: "Desenvolvedor",
+    linkedin: "",
+    github: "",
+  },
+  {
+    id: 6,
+    nome: "Welington Miranda",
+    foto: "./assets/photos/welington.png",
+    funcao: "Desenvolvedor",
+    linkedin: "",
+    github: "",
+  },
+]
+
+export default equipe;
---
+++ js/script.js
@@ -0,0 +1,18 @@
+// import equipe from './api/equipe.js';
+
+// const arcticlePessoa = document.querySelector('.pessoa');
+
+// equipe.map(item => {
+//   const nome = document.createElement('h2')
+//   nome.append(item.nome)
+//   const foto = document.createElement('img');
+//   foto.setAttribute("src", item.foto)
+//   foto.setAttribute("class", "esssa e uma classe")
+//   const funcao = document.createElement('p')
+//   funcao.append(item.funcao)
+
+//   arcticlePessoa.append(nome);
+//   arcticlePessoa.append(foto);
+//   arcticlePessoa.append(funcao);
+// });
+
